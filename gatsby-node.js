27ac45f4b7e458034e5b1969e99c8b050f4838ee
Dave Lindberg <davelindb@gmail.com>
2020-12-30 15:46:15 -0500

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,37 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+    const { createPage } = actions
+    return graphql(`
+        {
+            allWpPost(sort: {fields: [date] }) {
+                nodes {
+                    title
+                    excerpt
+                    content
+                    slug
+                }
+            }
+        }
+    `).then((result) => {
+        result.data.allWpPost.nodes.forEach((node) => {
+            createPage({
+                path: node.slug,
+                component: path.resolve(`./src/templates/blog-post.js`),
+                context: {
+                    // This is the $slug variable
+                    // passed to blog-post.js
+                    slug: node.slug,
+                },
+            })
+        })
+    })
+}
